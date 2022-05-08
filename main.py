537040db11b30961d4955a3b935dacacf3ea445e
choejaeho <kumoh2@naver.com>
2022-05-09 02:18:37 +0900

    TEST : init code test add

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import cx_Oracle
 
+OracleConnect = cx_Oracle.connect("scott/tiger@localhost")
+OracleCursor = OracleConnect.cursor()
 
 def no_space(text):
     text2 = text.text.strip()
@@ -20,20 +23,39 @@ while True:
     li = score_result.find_all('li')
 
     for i in range(len(li)):
-        usr_id = score_result.li.find_all_next('dt')
-        review_score = score_result.find_all(class_='star_score')
-        review_contents = score_result.li.find_all_next('p')
-        write_date = score_result.find_all(class_='score_reple')
-        up_count = score_result.li.a
-        down_count = score_result.li.a
+        usr_id_temp = score_result.li.find_all_next('dt')
+        usr_id = usr_id_temp[i].span.text
+
+        review_score_temp = score_result.find_all(class_='star_score')
+        review_score = review_score_temp[i].em.text
+
+        review_contents_temp = score_result.li.find_all_next('p')
+        review_contents = review_contents_temp[i].span.text.strip()
+
+        write_date_temp = score_result.find_all(class_='score_reple')
+        write_date = write_date_temp[i].em.next_sibling.next_sibling.text
+
+        up_count_temp = score_result.li.a
+        up_count = up_count_temp.find_all_next("strong")[2 * i].text
+
+        down_count_temp = score_result.li.a
+        down_count = down_count_temp.find_all_next("strong")[2*i+1].text
+
+        oracleSql = f""" 
+            INSERT INTO movie_review_scrap(movie_code,review_score,review_contents,usr_id,write_date,up_count,down_count)
+            VALUES({movie_code},{review_score},{review_contents},{usr_id},{write_date},{up_count},{down_count})
+        """
+
+        # print(oracleSql)
+        OracleCursor.execute(oracleSql)
 
         print("영화 :", movie_code)
-        print("아이디 :", usr_id[i].span.text)
-        print("평점 :", review_score[i].em.text)
-        print("리뷰 내용 :", review_contents[i].span.text.strip())
-        print("작성 날짜 :", write_date[i].em.next_sibling.next_sibling.text)
-        print("공감 숫자 :", up_count.find_all_next("strong")[2*i].text)
-        print("비공감 숫자 :", down_count.find_all_next("strong")[2*i+1].text)
+        print("아이디 :", usr_id)
+        print("평점 :",review_score)
+        print("리뷰 내용 :", review_contents)
+        print("작성 날짜 :", write_date)
+        print("공감 숫자 :", up_count)
+        print("비공감 숫자 :", down_count)
         print("페이지 넘버 :", movie_page)
         print(i,len(li))
         print("======================")
