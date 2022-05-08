8e83dc5f83d69ceeb89fc63c59f07ed0277c18a3
choejaeho <kumoh2@naver.com>
2022-05-08 22:40:13 +0900

    TEST : init code add

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-# 샘플 Python 스크립트입니다.
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
 
-# ⌃R을(를) 눌러 실행하거나 내 코드로 바꿉니다.
-# 클래스, 파일, 도구 창, 액션 및 설정을 어디서나 검색하려면 ⇧ 두 번을(를) 누릅니다.
+url = "https://movie.naver.com/movie/bi/mi/pointWriteFormList.naver?code=10001&page=999999](https://movie.naver.com/movie/bi/mi/pointWriteFormList.naver?code=10001&page=999999)"
 
+html = urlopen(url).read()
 
-def print_hi(name):
-    # 스크립트를 디버그하려면 하단 코드 줄의 중단점을 사용합니다.
-    print(f'Hi, {name}')  # 중단점을 전환하려면 ⌘F8을(를) 누릅니다.
+soup = BeautifulSoup(html, 'html.parser')
+html_class = soup.find_all(class_='star_score')
 
-
-# 스크립트를 실행하려면 여백의 녹색 버튼을 누릅니다.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# https://www.jetbrains.com/help/pycharm/에서 PyCharm 도움말 참조
+print(html_class)
